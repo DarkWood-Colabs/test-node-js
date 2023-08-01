be604c343b7b5e1e8a32b2dca7704e0d62411158
dillonbreytenach <dillon.nixlyn@gmail.com>
2023-08-01 05:22:04 -0700

    Here Goes NaDA

---
+++ testpage/index.js
@@ -0,0 +1,41 @@
+// index.js
+
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const axios = require('axios');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('chatMessage', async (message) => {
+    try {
+	    const apiKey = 'YOUR_API_KEY'; // Replace this with your actual API key
+	    const apiResponse = await axios.post('https://api.openai.com/v1/conversations/text-davinci-003/messages', {
+		    message: message,
+		    apiKey: apiKey
+      },
+      const responseMessage = apiResponse.data.message;
+      io.emit('message', responseMessage);
+    } catch (error) {
+      console.error('Error calling the API:', error.message);
+      io.emit('message', 'Error: Could not process your message.');
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
